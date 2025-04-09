55edcd88d6326b6842b06eb69fa0dfea8d345a5e
MaxHuang <hc18174419502@163.com>
2025-04-09 14:46:53 +0800

    add code

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+import { IsNotEmpty } from "class-validator";
+
+export class CreateUserDto {
+  @IsNotEmpty({ message: "手机号不能为空" })
+  phone_number: string;
+  @IsNotEmpty({ message: "密码不能为空" })
+  password: string;
+  @IsNotEmpty({ message: '验证码不能为空' })
+  smsCode: string;
+}

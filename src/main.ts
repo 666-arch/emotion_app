df1c78d560ad86b0be1c732a926fc5e9ec83bbed
MaxHuang <hc18174419502@163.com>
2025-04-10 14:24:15 +0800

    add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import * as process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('emotion_api_doc')
+    .setDescription('emotion_app_api_document')
+    .setVersion('1.0.0')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, documentFactory);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
